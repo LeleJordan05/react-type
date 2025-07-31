948dbaab6bf8857085093bc220a7666a90b2c28b
LeleJordan05 <emanueleg656@gmail.com>
2025-07-31 15:57:58 +0200

    modifica hook

--- src/hooks/useArticles.ts
+++ src/hooks/useArticles.ts
@@ -1,69 +1,44 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { CreateArticlePayload, UpdateArticlePayload } from "../types/Article";
 import { Api } from "../api/Api";
-import { useArticleStore } from "../store/articleStore";
-import { useEffect } from "react";
-import { ArticleState } from "../store/articleStore";
 
 export function useArticles() {
-  const setArticles = useArticleStore(
-    (state: ArticleState) => state.setArticles
-  );
-
-  const query = useQuery({
+  return useQuery({
     queryKey: ["articles"],
     queryFn: Api.getArticles,
   });
-
-  useEffect(() => {
-    if (query.data) {
-      setArticles(query.data);
-    }
-  }, [query.data]);
-
-  return query;
 }
 
 export function useCreateArticle() {
   const queryClient = useQueryClient();
-  const addArticle = useArticleStore((state: ArticleState) => state.addArticle);
 
   return useMutation({
     mutationFn: (data: CreateArticlePayload) => Api.createArticle(data),
-    onSuccess: (newArticle) => {
-      queryClient.invalidateQueries({ queryKey: ["articles"] }),
-        addArticle(newArticle);
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["articles"] });
     },
   });
 }
 
 export function useUpdateArticle() {
   const queryClient = useQueryClient();
-  const updateArticle = useArticleStore(
-    (state: ArticleState) => state.updateArticle
-  );
 
   return useMutation({
     mutationFn: ({ id, data }: { id: string; data: UpdateArticlePayload }) =>
       Api.updateArticle(id, data),
-    onSuccess: (updatedArticle) => {
-      queryClient.invalidateQueries({ queryKey: ["articles"] }),
-        updateArticle(updatedArticle.id, updatedArticle);
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["articles"] });
     },
   });
 }
 
 export function useDeleteArticle() {
   const queryClient = useQueryClient();
-  const removeArticle = useArticleStore(
-    (state: ArticleState) => state.removeArticle
-  );
 
   return useMutation({
     mutationFn: (id: string) => Api.deleteArticle(id),
-    onSuccess: (_, id) => {
-      queryClient.invalidateQueries({ queryKey: ["articles"] }),
-        removeArticle(id);
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["articles"] });
     },
   });
 }
--- src/hooks/useUsers.ts
+++ src/hooks/useUsers.ts
@@ -1,61 +1,44 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { Api } from "../api/Api";
 import { CreateUserPayload, UpdateUserPayload } from "../types/User";
-import { useUserStore } from "../store/userStore";
-import { useEffect } from "react";
-import { UserState } from "../store/userStore";
 
 export function useUsers() {
-  const setUsers = useUserStore((state: UserState) => state.setUsers);
-
-  const query = useQuery({
+  return useQuery({
     queryKey: ["users"],
     queryFn: Api.getUsers,
   });
-
-  useEffect(() => {
-    if (query.data) {
-      setUsers(query.data);
-    }
-  }, [query.data]);
-
-  return query;
 }
 
 export function useCreateUser() {
   const queryClient = useQueryClient();
-  const addUser = useUserStore((state: UserState) => state.addUser);
 
   return useMutation({
     mutationFn: (data: CreateUserPayload) => Api.createUser(data),
-    onSuccess: (newUser) => {
-      queryClient.invalidateQueries({ queryKey: ["users"] }), addUser(newUser);
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["users"] });
     },
   });
 }
 
 export function useUpdateUser() {
   const queryClient = useQueryClient();
-  const updateUser = useUserStore((state: UserState) => state.updateUser);
 
   return useMutation({
     mutationFn: ({ id, data }: { id: string; data: UpdateUserPayload }) =>
       Api.updateUser(id, data),
-    onSuccess: (updatedUser) => {
-      queryClient.invalidateQueries({ queryKey: ["users"] }),
-        updateUser(updatedUser.id, updatedUser);
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["users"] });
     },
   });
 }
 
 export function useDeleteUser() {
   const queryClient = useQueryClient();
-  const removeUser = useUserStore((state: UserState) => state.removeUser);
 
   return useMutation({
     mutationFn: (id: string) => Api.deleteUser(id),
-    onSuccess: (_, id) => {
-      queryClient.invalidateQueries({ queryKey: ["users"] }), removeUser(id);
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["users"] });
     },
   });
 }
