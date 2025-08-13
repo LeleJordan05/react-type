86d542c35371f5d34e632b905340cb3958487eb8
LeleJordan05 <emanueleg656@gmail.com>
2025-08-13 15:22:39 +0200

    card2.0

--- src/components/myComponents/cardlist.tsx
+++ src/components/myComponents/cardlist.tsx
@@ -10,7 +10,6 @@ import {
 } from "../ui/dialog";
 import { EditBook, EditUser } from "./edit";
 import { DeleteBook, DeleteUser } from "./delete";
-
 export function UserCard() {
   return (
     <Card className="card">
@@ -60,23 +59,39 @@ export function BookCard() {
           className="!h-25 !rounded-lg"
         />
         <p className=" !text-black/80 font-medium !ml-5">Cenerentola</p>
-        <div className="!ml-140">
+        <div className="!ml-110">
           <Dialog>
             <DialogTrigger asChild className="viewbooks">
-              <Button variant="outline">Libri</Button>
+              <Button variant="outline">Descrizione</Button>
             </DialogTrigger>
             <DialogContent className="!p-6 !h-auto">
               <DialogHeader>
-                <DialogTitle className="!font-medium">
-                  Libri in vendita
-                </DialogTitle>
+                <DialogTitle className="!font-medium">Descrizione</DialogTitle>
                 <DialogDescription>
-                  Apporta qui i dati del tuo libro. Clicca su Aggiungi quando
-                  hai finito.
+                  Quis commodi libero reiciendis fugiat. Nostrum maiores et et
+                  pariatur earum. Nulla perferendis consequatur. Quisquam enim
+                  nihil neque deserunt. Voluptatem nihil labore nam numquam.
+                  Voluptatem aliquam sequi deleniti nobis alias
+                  mollitia.\nPerspiciatis in esse aut. Quod sed odio sunt eius
+                  assumenda perferendis. Eum et corrupti esse quia aut eos.
+                  Consequatur nobis voluptas soluta non et omnis quaerat
+                  dolorem.\nNisi vero distinctio unde mollitia. Vel deserunt
+                  quos maiores laborum aut inventore ducimus perspiciatis. Quo
+                  ipsa sed in et perspiciatis ducimus quisquam totam dicta.
+                  Blanditiis dicta odio. Necessitatibus ratione mollitia et
+                  natus natus corporis.
                 </DialogDescription>
               </DialogHeader>
             </DialogContent>
           </Dialog>
+        </div>
+        <div>
+          <button
+            onClick={() => window.open("https://sscnapoli.it/", "_blank")}
+            className="viewbooks !ml-5"
+          >
+            Acquista
+          </button>
         </div>
         <div className="!ml-5">
           <EditBook></EditBook>
